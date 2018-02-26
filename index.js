98f79594576419694b057cf61b994931516f887c
Jake Overall <joverall22@gmail.com>
2018-02-26 12:17:48 -0700

    adds basic auth

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-var express = require('express');
-var bp = require('body-parser');
-var cors = require('cors');
+var express = require("express");
+var bp = require("body-parser");
+var cors = require("cors");
 var server = express();
-require('./server-assets/db/mlab-config');
+require("./server-assets/db/mlab-config");
 var port = 3000;
-var shipRoutes = require('./server-assets/routes/ships');
-var logRoutes = require('./server-assets/routes/logs');
-var commentRoutes = require('./server-assets/routes/comments');
-
+var authRoutes = require("./server-assets/auth/routes");
+var shipRoutes = require("./server-assets/routes/ships");
+var logRoutes = require("./server-assets/routes/logs");
+var commentRoutes = require("./server-assets/routes/comments");
 
 server.use(cors());
 server.use(bp.json());
-server.use(bp.urlencoded({extended: true}));
+server.use(bp.urlencoded({ extended: true }));
+
+server.use(authRoutes);
+
 server.use(shipRoutes.router);
 server.use(logRoutes.router);
 server.use(commentRoutes.router);
 
-
-
-
-
-
-server.use('*', (err,req,res,next) =>{
+server.use("*", (err, req, res, next) => {
   res.status(400).send(err);
 });
 
-server.listen(port, ()=>{
+server.listen(port, () => {
   console.log("Server running on port: ", port);
 });
